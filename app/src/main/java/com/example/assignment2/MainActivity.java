998745d8e0695f908c69358b133439f29e8f6eff
hercules2123 <a1523709@gmail.com>
2022-04-13 18:27:47 +0800

    test3

--- app/src/main/java/com/example/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.assignment2;
 
 import android.os.CountDownTimer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Locale;
 
